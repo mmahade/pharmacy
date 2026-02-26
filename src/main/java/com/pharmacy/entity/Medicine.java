8962e8b09a781fea59a629572d3676405141950e
Asad Ullah <au10050305@gmail.com>
2026-02-26 13:03:23 +0600

    Refactor Medicine entity to use Long for ID and simplify imports

--- src/main/java/com/pharmacy/entity/Medicine.java
+++ src/main/java/com/pharmacy/entity/Medicine.java
@@ -1,15 +1,6 @@
 package com.pharmacy.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,8 +22,8 @@ import java.util.UUID;
 public class Medicine {
 
     @Id
-    @GeneratedValue
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false)
     private String name;
